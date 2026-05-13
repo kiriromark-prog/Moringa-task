15f141e5c4b4989dd1968a1018d810ab7f6618f0
kiriromark.prog <kiriromark@gmail.com>
2026-05-13 09:22:07 +0300

    First push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginPage from './login';
+import Signup from './signup';
+function App() {
+  
+  return (
+<>
+     <LoginPage/>
+     <Signup/>
+
+      
+</>
+  );
+}
+
+
+export default App;
